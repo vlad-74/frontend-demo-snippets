de96a9746b880913bffc0e90056eb61a8ab27669
Vladimi Kurguzov <kurguzov74@gmail.com>
2017-11-26 09:52:23 +0300

    2611 commit

--- demo/bat_cmd_PowerShel/dir/index.js
+++ demo/bat_cmd_PowerShel/dir/index.js
@@ -6,11 +6,42 @@ var open = require("open");
 
 open(__dirname + "/mongodb.bat");
 
-console.log('ДЛЯ НОРМАЛЬНОЙ РАБОТЫ СКРИПТА НУЖНО : \n' 
-	+ '1. ЧТОБЫ БЫЛА УСТАНОВЛЕНА mongodb. ПУТЬ УСТАНОВЛЕННОЙ mongodb = C:\\mongodb\\bin \n'
-	+ '2. ....И МОЖЕТ БЫТЬ, В ПЕРЕМЕННУЮ СРЕДЫ Path ДОБАВИТЬ ПУТЬ К ФАЙЛАМ mongodb = C:\\mongodb\\bin \n'
-	+ '3. ЗАПУСКАТЬ МОЖНО ВРУЧНУЮ - 2 РАЗА КЛИКНУТЬ ПО mongodb.bat \n'
-	+ '4. mongodb ЗАПУСТИТСЯ ЧЕРЕЗ 9 - 10 СЕКУНД');
+console.log(`ДЛЯ НОРМАЛЬНОЙ РАБОТЫ СКРИПТА НУЖНО :
+	1. ЧТОБЫ БЫЛА УСТАНОВЛЕНА mongodb. ПУТЬ УСТАНОВЛЕННОЙ mongodb = C:\\mongodb\\bin
+	2. ....И МОЖЕТ БЫТЬ, В ПЕРЕМЕННУЮ СРЕДЫ Path ДОБАВИТЬ ПУТЬ К ФАЙЛАМ mongodb = C:\\mongodb\\bin
+	3. ЗАПУСКАТЬ МОЖНО ВРУЧНУЮ - 2 РАЗА КЛИКНУТЬ ПО mongodb.bat 
+	4. mongodb ЗАПУСТИТСЯ ЧЕРЕЗ 9 - 10 СЕКУНД
+
+	СОЗДАНИЕ ПАПКИ:
+	mkdir folder
+
+	СОЗДАНИЕ ПУСТОГО ФАЙЛА:
+	copy NUL EmptyFile.js
+	ЗАПИСЬ В СУЩЕСТВУЮЩИЙ ФАЙЛ:
+	echo SOME TEXT > EmptyFile.js
+
+	ЗАПИСЬ В СОЗДАВАЕМЫЙ ФАЙЛ:
+	echo SOME TEXT > notEmptyFile.js
+
+	ЧТЕНИЕ ФАЙЛА:
+	more notEmptyFile.js
+
+	-----------------------------------------
+	bush
+	ВСЕ АЛИСЫ (ОТКРЫВАТЬ ОТ АДМИНИСТРАТОРА ЧЕРЕЗ РЕДАКТОР) - C:/Program Files/Git/etc/profile.d/aliases.sh 
+	alias cls="clear; printf '\\033[3J'"
+
+	СОЗДАНИЕ ПУСТОГО ФАЙЛА:
+	touch index.html
+
+	ЗАПИСЬ В СОЗДАВАЕМЫЙ ФАЙЛ:
+	echo SOME TEXT > index.html
+
+	ЧТЕНИЕ ФАЙЛА
+	less
+	vim
+	`
+	);
 
 console.log(gl_hr + '\nВЫВЕСТИ СОДЕРЖИМОЕ БАТНИКА (mongodb.bat)');
   hf.fil.fileTostring(__dirname + "/mongodb.bat")
--- demo/bower/dir/index.js
+++ demo/bower/dir/index.js
@@ -10,14 +10,26 @@ let str = `
 Установка Бовера bower@1.8.2 (потребуются Node.js и Git). 
 npm install -g bower
 
+bower -v
+
 Создадим bower.json:
 bower init
 
+Поиск пакетов:
+bower search jquery
+???открыть в браузере bower.io/search/
+
 Попробуем что-нибудь установить, например jQuery:
 bower install --save jquery
+bower i jquery -S
+bower install jquery#1.6.4
+bower i normalize.css
 bower install desandro/masonry
 bower install git://github.com/user/package.git // Git endpoint
 bower install http://example.com/script.js // URL
+
+Можно устанавливать несколько пакетов сразу:
+bower install --save social-likes jquery-icheck fotorama
 
 Флаг --save говорит Боверу, что он должен сохранить имя пакета и его версию в 
 специальный файл со списком всех зависимостей проекта — bower.json
@@ -26,28 +38,110 @@ bower install http://example.com/script.js // URL
 Тильда в начале говорит о том, что при установке будет выбрана версия 1.0.1 
 или с большим последним числом (ПАТЧ), если она есть
 
-Можно устанавливать несколько пакетов сразу:
-bower install --save social-likes jquery-icheck fotorama
-
-Поиск пакетов:
-bower search jquery
-открыть в браузере bower.io/search/
+Список установленных библиотек
+bower list покажет список всех установленных пакетов. Ты увидишь, что все пакеты 
+зависят от jQuery и что Бовер смог найти удовлетворяющую всех версию jQuery
+bower list --path
 
 Для обновления пакетов (и bower.json) до действительно последних версий можно воспользоваться 
 npm install -g bower-update
 Запускаем: bower-update
 
 В Бовере есть команда bower update, но она обновляет пакеты с учетом требований, описанных в 
-ower.json. Например, если там требуется jQuery ~2.0.0, то он сможет обновить его, например, 
-до 2.0.9, но 2.1.0 уже не поставит
+bower.json. Например, если там требуется :
+jQuery ~2.0.0, то он сможет обновить его, например, до 2.0.9, но 2.1.0 уже не поставит
+jQuery ^1.0.4, то он сможет обновить его, например, до 1.11.3, но 2.0.0 уже не поставит
+А если заменить ^ на latest, то получим версию 2.1.4
 
 Для удаления пакетов используется команда:
 bower uninstall --save packageName
 
-bower list покажет список всех установленных пакетов. Ты увидишь, что все пакеты 
-зависят от jQuery и что Бовер смог найти удовлетворяющую всех версию jQuery
-
 Ты можешь спокойно удалять папку bower_components или добавить ее в свой .gitignore. 
 Команда bower install (без дополнительных параметров) вернет все как было
+
+По-умолчанию, bower устанавливает все зависимости в папку bower_components. Однако, мы 
+можем поменять как имя папки, так и её место расположение. Делается это очень просто: 
+необходимо создать рабочий файл, который должен называться .bowerrc
+{
+	"directory" : "app/bower"
+}
+Теперь перенесем уже существующую папку в новое место. Для этого в консоли наберем
+mkdir app
+mv bower_components app/bower
+
+====================================================================================================
+gulp-google-cdn
+Данный плагин заменяет ваши javascript библиотеки подключенные локально на аналогичные из google-cdn.
+
+npm i gulp-google-cdn -D
+
+gulpfile.js
+var gulp = require('gulp');
+var googlecdn = require('gulp-google-cdn');
+
+gulp.task('default', function () {
+    return gulp.src('index.html')
+        .pipe(googlecdn(require('./bower.json')))
+        .pipe(gulp.dest('dist'));
+});
+bower.json
+{
+  "name": "app",
+  "dependencies": {
+    "jquery": "~2.1.1"
+  }
+}
+
+--------------------------------------------------------------------------------------------------
+main-bower-files
+Данный плагин поможет нам извлечь из всех сотен файлов скаченных bower’ом в папку 
+bower_components только нужные нам файлы.
+
+npm i main-bower-files -D
+
+gulpfile.js
+var gulp = require('gulp');
+var mainBowerFiles = require('main-bower-files');
+
+gulp.task('mainfiles', function() {
+    return gulp.src(mainBowerFiles())
+    .pipe(gulp.dest('dist/mainfiles')) //Обязательно укажем место назначения — куда будут складываться главные файлы
+});
+
+Переопределим главные файлы для bootstrap прямо внутри bower.json
+"overrides": {
+  "bootstrap": {
+      "main": [ //пути указанные в bower.json bootstrap’а
+          "./dist/js/bootstrap.min.js",
+          "./dist/css/bootstrap.min.css",
+          "./dist/css/bootstrap-theme.min.css"
+          ]
+  }
+}
+--------------------------------------------------------------------------------------------------
+
+Preen
+Preen просто-напросто уничтожает все неиспользуемые файлы из bower_components, оставляя только те файлы, 
+которые мы сами его попросим оставить.
+
+Preen нужно установить глобально
+
+bower.json
+  {
+  	"name": "app",
+  	"dependencies": {
+  		"jquery": "~2.1.1"
+  	},
+  "preen": {
+      "jquery": [
+        "dist/*.js"
+      ]
+    }
+  }
+Далее, всё, что нам нужно — это просто написать в консоли команду
+preen
+
+
+
 `
 console.log(str);
